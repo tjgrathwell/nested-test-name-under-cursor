72d5f0941c5151c1fcf7d029dc792880890869b3
Travis Grathwell <tjgrathwell@gmail.com>
2016-10-13 16:34:04 -0700

    Allow file+line combo to be specified with a colon, e.g. /file/name:line

--- bin/nested-test-name-under-cursor.js
+++ bin/nested-test-name-under-cursor.js
@@ -5,24 +5,31 @@
 const path = require('path');
 
 function printUsage() {
+  console.log("Usage:");
   console.log(
-    "Usage:",
+    ' ',
     path.basename(process.argv[1]),
     "file_name",
     "line_number"
+  );
+  console.log(
+    ' ',
+    path.basename(process.argv[1]),
+    "file_name:line_number"
   );
 }
 
 var jasmineTestUnderCursor = require('../src/nested-test-name-under-cursor');
 
 var userArgs = process.argv.slice(2);
-if (userArgs < 2) {
-  printUsage();
-  return;
-}
-
 var fileName = userArgs[0];
 var lineNumber = userArgs[1];
+if (userArgs.length === 1 && userArgs[0].indexOf(':') !== -1) {
+  var parts = userArgs[0].split(':');
+  fileName = parts[0];
+  lineNumber = parts[1];
+}
+
 if (!fileName || !lineNumber) {
   printUsage();
   return;
